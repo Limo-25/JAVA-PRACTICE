406da5d474ccdd96d5d284f7aa8248ac500b4723
Limo-25 <arijitd2510@gmail.com>
2026-09-07 21:32:41 +0530

    Manipulating array values using functions

---
+++ Arrays/ValueUpdation.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class ValueUpdation {
+
+    public static void main(String[] args) {
+
+        int marks[] = {97, 98, 99};
+
+        update(marks);
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.print(marks[i] + " ");
+        }
+    }
+
+    public static void update(int[] marks) {
+
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = marks[i] + 1;
+        }
+    }
+}
