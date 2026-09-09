ca313cb15cf66cc8d6f653e3ba7588dbb8d85280
Limo-25 <arijitd2510@gmail.com>
2026-09-09 22:43:01 +0530

    Reverse the array

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,27 @@
+package Arrays;
+public class ReverseArray {
+
+    public static void main(String[] args) {
+         int Arr[]={1,2,6,7,10,12,34,55};
+         Reverse(Arr);
+
+         for(int i = 0; i < Arr.length ; i++ ){
+         System.out.print( Arr[i] + " ");
+         }
+    }
+
+    public static void Reverse(int Arr[]){
+        int start = 0 ;
+        int end = Arr.length - 1 ;
+
+        while (start < end) {
+            int temp = Arr[start];
+            Arr[start] = Arr[end];
+            Arr[end] = temp ;
+
+             start++ ;
+             end -- ;
+        }
+       
+    }
+}
